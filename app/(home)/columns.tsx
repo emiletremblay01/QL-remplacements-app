29a73ee2a2f85cc5e351170f49047e686bc75352
Emile Tremblay <emiletremblay01@gmail.com>
2023-12-01 03:37:18 -0500

    changed accepte

--- app/(home)/columns.tsx
+++ app/(home)/columns.tsx
@@ -71,7 +71,7 @@ export const columns: ColumnDef<Remplacement>[] = [
         <div
           className={cn("px-2 py-1 flex justify-center w-full rounded-full", {
             "bg-yellow-100 text-yellow-800": statut === "en attente",
-            "bg-green-100 text-green-800": statut === "accepté",
+            "bg-green-100 text-green-800": statut === "approuvé",
             "bg-red-100 text-red-800": statut === "refusé",
           })}
         >
