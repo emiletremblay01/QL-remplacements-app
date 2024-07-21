a8d75f1c7c9867d0c9bb024488491ebe84e101ab
Emile Tremblay <emiletremblay01@gmail.com>
2024-07-21 19:22:37 -0400

    fix: integrate react-csv package

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -2,15 +2,10 @@
 import { Button } from "@/components/ui/button";
 import { DownloadIcon } from "lucide-react";
 import { Remplacement } from "@/types";
-import Papa from 'papaparse';
+import Papa from "papaparse";
+import { CSVLink } from "react-csv";
 
-export const DownloadButton = ({
-  data,
-  fileName,
-}: {
-  data: Remplacement[];
-  fileName: string;
-}) => {
+export const DownloadButton = ({ data }: { data: Remplacement[] }) => {
   const formatDate = (date: Date): string => {
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
@@ -18,49 +13,24 @@ export const DownloadButton = ({
     return `${day}/${month}/${year}`;
   };
 
-  const formatDataForCSV = (data: Remplacement[]) => {
-    // Format data for PapaParse
-    const csvData = data.map(item => ({
-      "Date de la demande": formatDate(new Date(item.dateDemande)),
-      "Date du quart": formatDate(new Date(item.dateQuart)),
-      "Heures du quart": item.heuresQuart,
-      "Poste": item.posteQuart,
-      "Nom de l'equipier": item.nomEquipier,
-      "Nom de l'equipier remplaçant": item.nomEquipierRemplacant,
-      "Directeur": item.remplacementEffectuePar,
-    }));
+  const csvData = data.map((item) => ({
+    "Date de la demande": formatDate(new Date(item.dateDemande)),
+    "Date du quart": formatDate(new Date(item.dateQuart)),
+    "Heures du quart": item.heuresQuart,
+    Poste: item.posteQuart,
+    "Nom de l'equipier": item.nomEquipier,
+    "Nom de l'equipier remplaçant": item.nomEquipierRemplacant,
+    Directeur: item.remplacementEffectuePar,
+  }));
 
-    return Papa.unparse(csvData);
-  };
-
-  const handleDownload = () => {
-    const csvData = formatDataForCSV(data);
-    
-    // Create a Blob object for the CSV data
-    const blob = new Blob([csvData], { type: 'text/csv' });
-    
-    // Create a temporary URL for the Blob
-    const url = window.URL.createObjectURL(blob);
-    
-    // Create a temporary <a> element to trigger the download
-    const a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = url;
-    a.download = 'data.csv';  // Specify the file name
-    document.body.appendChild(a);
-    
-    // Programmatically click the <a> element to trigger the download
-    a.click();
-    
-    // Cleanup: remove the <a> element and revoke the URL
-    window.URL.revokeObjectURL(url);
-    document.body.removeChild(a);
-  };
+  const final = Papa.unparse(csvData);
 
   return (
-    <Button onClick={handleDownload}>
-      Télécharger
-      <DownloadIcon className="ml-2 h-4 w-4" />
+    <Button>
+      <CSVLink data={final}>
+        Télécharger
+        <DownloadIcon className="ml-2 h-4 w-4" />
+      </CSVLink>
     </Button>
   );
 };
