0501451c00d9a2952698b6ef67a4751f111ee899
Emile Tremblay <emiletremblay01@gmail.com>
2024-07-25 14:34:33 -0400

    fix: remove deprecated prop

--- app/(root)/(remplacements)/all-remplacements/page.tsx
+++ app/(root)/(remplacements)/all-remplacements/page.tsx
@@ -1,9 +1,3 @@
-// TODO: Add loading skeleton
-// TODO: Add Statut handling vs Courriel envoyé
-// TODO: Add toggle for couriel envoyé inside datatable
-// TODO: Change Affichage de colonnes columns names
-// TODO: Clean up code imports
-
 import { columns } from "../components/columns";
 import { DataTable } from "../components/data-table";
 
@@ -18,7 +12,7 @@ export default async function AllRemplacements() {
     <div className="container mx-auto py-10 space-y-2">
       <h1 className="font-semibold text-muted-foreground text-lg flex  justify-between gap-4 items-center">
         Historique de tous les remplacements
-        <DownloadButton fileName="remplacements" data={remplacements} />
+        <DownloadButton data={remplacements} />
       </h1>
       <DataTable columns={columns} data={remplacements} />
     </div>
